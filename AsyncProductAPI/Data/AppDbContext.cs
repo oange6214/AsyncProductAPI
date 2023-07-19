d4f1db278dd815864c53e07db466ad4e8fa9d63f
Jed <oange6214@gmail.com>
2023-07-20 00:25:01 +0800

    Add project files.

---
+++ AsyncProductAPI/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AsyncProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsyncProductAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+    {
+    }
+
+    public DbSet<ListingRequest> ListingRequests => Set<ListingRequest>();
+}
---
+++ AsyncProductAPI/Dtos/ListingStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace AsyncProductAPI.Dtos;
+
+public class ListingStatus
+{
+    public string? RequestStatus { get; set; }
+    public string? EstimatedCompetionTime { get; set; }
+    public string? ResourceURL { get; set; }
+}
